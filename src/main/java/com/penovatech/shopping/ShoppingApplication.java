0f97c1a55f8b598510601ff856af41a212aff335
barzegarallahverdi.m <barzegarallahverdi.m@tiddev.com>
2025-02-11 13:47:13 +0330

    . add messageSource bean

--- src/main/java/com/penovatech/shopping/ShoppingApplication.java
+++ src/main/java/com/penovatech/shopping/ShoppingApplication.java
@@ -2,7 +2,9 @@ package com.penovatech.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.penovatech.shopping")
@@ -10,6 +12,17 @@ public class ShoppingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingApplication.class, args);
+	}
+
+	@Bean(name = "messageSource")
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames(
+				"classpath:common_exception_message",
+				"classpath:shopping_message");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setFallbackToSystemLocale(false);
+		return messageSource;
 	}
 
 }
