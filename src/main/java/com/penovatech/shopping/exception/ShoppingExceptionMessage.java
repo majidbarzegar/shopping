7df16844943bcb8c5f69fc9fcfb53d7ed0ee0ef2
Majid <majidbarzegar.a@gmail.com>
2025-02-23 18:25:51 +0000

    . change in error

--- src/main/java/com/penovatech/shopping/exception/ShoppingExceptionMessage.java
+++ src/main/java/com/penovatech/shopping/exception/ShoppingExceptionMessage.java
@@ -3,20 +3,18 @@ package com.penovatech.shopping.exception;
 import com.penovatech.common.exception.ExceptionMessage;
 
 public enum ShoppingExceptionMessage implements ExceptionMessage {
-    CAN_NOT_SAVE_FILE(1001, "CAN_NOT_SAVE_FILE"),
-    INVALID_JSON_INPUT(1002, "INVALID_JSON_INPUT"),
-    INVALID_USERNAME_OR_PASSWORD(1003, "INVALID_USERNAME_OR_PASSWORD"),
-    USER_NOT_AUTHORIZATION(1004, "USER_NOT_AUTHORIZATION"),
-    CAN_NOT_DELETE_FILE(1005, "CAN_NOT_DELETE_FILE"),
-    USER_WITH_THIS_EMAIL_IS_EXIST(1006, "USER_WITH_THIS_EMAIL_IS_EXIST"),
+    CAN_NOT_SAVE_FILE(1001),
+    INVALID_JSON_INPUT(1002),
+    INVALID_USERNAME_OR_PASSWORD(1003),
+    USER_NOT_AUTHORIZATION(1004),
+    CAN_NOT_DELETE_FILE(1005),
+    USER_WITH_THIS_EMAIL_IS_EXIST(1006),
     ;
 
     private Integer code;
-    private String messageKey;
 
-    ShoppingExceptionMessage(Integer code, String messageKey) {
+    ShoppingExceptionMessage(Integer code) {
         this.code = code;
-        this.messageKey = messageKey;
     }
 
     @Override
@@ -26,7 +24,7 @@ public enum ShoppingExceptionMessage implements ExceptionMessage {
 
     @Override
     public String getMessageKey() {
-        return messageKey;
+        return this.name();
     }
 
 }
